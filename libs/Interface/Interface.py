30c9c16bcc83e1e233f5b58307b818465248b601
MOHAMED OUAICHA <ouaicha47@gmail.com>
2024-08-21 22:00:35 +0100

    Authenticate to HugginFace only if the user choose it

--- libs/Interface/Interface.py
+++ libs/Interface/Interface.py
@@ -12,29 +12,29 @@ from libs.utils.logging_config import setup_logging
 def main():
     setup_logging()
     logger = logging.getLogger(__name__)
-
-    load_dotenv()
-    TOKEN = os.getenv("HF_TOKEN")
-    if not TOKEN:
-        logger.error("Hugging Face token not found in environment variables")
-        return
-    login(token=TOKEN, add_to_git_credential=True)
-    logger.info("Logged into Hugging Face Hub")
-
+    logger.info("Starting the application")
     # get a file from the user
     uploaded_file = st.file_uploader("Choose a file", type=["pdf", "docx"])
     model_name = st.selectbox(
         "Choose a model", ["google/gemma-2-9b", "meta-llama/Meta-Llama-3.1-8B-Instruct"]
     )
     type_of_analysis = st.radio(
-        "Choose the type of analysis", ["HuggingFace API", "Ollama Locally"]
+        "Choose the type of analysis", ["Ollama Locally (Recommended)", "HuggingFace API"], index=0
     )
     button = st.button("Process Resume")
 
     if uploaded_file and button:
         if type_of_analysis == "HuggingFace API":
+            load_dotenv()
+            TOKEN = os.getenv("HF_TOKEN")
+            if not TOKEN:
+                logger.error("Hugging Face token not found in environment variables")
+                return
+            login(token=TOKEN, add_to_git_credential=True)
+            logger.info("Logged into Hugging Face Hub successfully")
             structured_resume = HuggingFace_API(uploaded_file, model_name)
         elif type_of_analysis == "Ollama Locally":
+            logger.info("Starting Ollama Locally")
             structured_resume = Ollama_Locally(uploaded_file, model_name)
         st.write(structured_resume)
         logger.info("Resume processed successfully")
